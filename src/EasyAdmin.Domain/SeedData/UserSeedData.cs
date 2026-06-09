3e1e40ec8d693e6ea0cab6bd7d9f4634564250cf
Lu Weixian <weixian.lu@foxmail.com>
2026-06-09 11:25:38 +0800

    新增部门\岗位种子数据，调整代码生成菜单位置

--- src/EasyAdmin.Domain/SeedData/UserSeedData.cs
+++ src/EasyAdmin.Domain/SeedData/UserSeedData.cs
@@ -1,4 +1,4 @@
-﻿using EasyAdmin.Domain.Contracts;
+using EasyAdmin.Domain.Contracts;
 using EasyAdmin.Domain.Entities;
 using EasyAdmin.Infrastructure.Const;
 using EasyAdmin.Infrastructure.Enums;
@@ -22,7 +22,7 @@ public class UserSeedData : IEntitySeedData<UserEntity>
             new UserEntity { TenantId = SysConst.DefaultTenantId, Id = SysConst.DefaultTenantAdminUserId, UserName = "admin", Password = hash.MD5("admin").ToLower(), NickName = "管理员", State = CommonState.Enable },
             new UserEntity { TenantId = SysConst.DefaultTenantId, Id = 3, UserName = "user01", Password = defaultPwd, NickName = "普通用户01", State = CommonState.Enable },
             new UserEntity { TenantId = SysConst.DefaultTenantId, Id = 4, UserName = "user02", Password = defaultPwd, NickName = "普通用户02", State = CommonState.Enable },
-            new UserEntity { TenantId = SysConst.DefaultTenantId, Id = 10, UserName = "Sean", Password = defaultPwd, NickName = "大师兄", Email = "weixian.lu@foxmail.com", State = CommonState.Enable },
+            new UserEntity { TenantId = SysConst.DefaultTenantId, Id = 10, UserName = "Sean", Password = defaultPwd, NickName = "大师兄", Email = "weixian.lu@foxmail.com", DepartmentId = 1000, PositionId = 1, State = CommonState.Enable },
         };
     }
 }
